baf152e1c8795260555844dd91b3c4fbeaaafd85
Dmitry Smolyakov <dm.smolyakov@gmail.com>
2018-10-06 01:15:33 +0300

    Settings updated

--- Sources/App/Configuration/configure.swift
+++ Sources/App/Configuration/configure.swift
@@ -17,9 +17,16 @@ public func configure(
     services.register(middlewares)
     
     var databases = DatabasesConfig()
-    let config = MySQLDatabaseConfig(hostname: "10.0.10.71", username: "hAQGfmoz+KiqjSm", database: "hAQGfmoz+KiqjSm")
+    let database = Environment.get("DATABASE_DB") ?? "vapor"
+    print("database: \(database)")
+    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+    print("hostname: \(database)")
+    let username = Environment.get("DATABASE_USER") ?? "vapor"
+    print("username: \(database)")
+    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+    print("password: \(database)")
+    let config = MySQLDatabaseConfig(hostname: hostname, username: username, password: password, database: database)
 //    let config = MySQLDatabaseConfig(hostname: "localhost", username: "root", password: "", database: "chatter", transport: MySQLTransportConfig.unverifiedTLS)
-    
     databases.add(database: MySQLDatabase(config: config), as: .mysql)
     services.register(databases)
     
